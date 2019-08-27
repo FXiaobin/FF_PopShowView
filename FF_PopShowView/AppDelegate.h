1bd5a820ad9571f8b58739fa601343f4b1da58fe
Frankfan@healife.com <Frankfan@healife.com>
2019-08-27 18:06:36 +0800

    Initial Commit

---
+++ FF_PopShowView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FF_PopShowView
+//
+//  Created by mac on 2019/8/27.
+//  Copyright © 2019 healifeGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FF_PopShowView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FF_PopShowView
+//
+//  Created by mac on 2019/8/27.
+//  Copyright © 2019 healifeGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
